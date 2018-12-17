2e8de4ddec59a79b81f8ab25f9b9c2c1600c0fe6
Richie <richiemck47@gmail.com>
2018-12-16 23:48:30 -0700

    change webscrape source

--- html_test.py
+++ html_test.py
@@ -1,21 +1,20 @@
-from player_storage import Playerbase
 from bs4 import BeautifulSoup
-import re
 import requests
+from player_storage import Playerbase
 
-#In the future I'll need to figure out how to call a download of the html page then perform the actions vs
-#saving locally! (Imagine having to update manually for all 30 teams ew.)
-page = r"C:\Users\Richie\Documents\VSC_Files\suns_dump.html" #put r in front to make a 'raw str' to read path
-soup = BeautifulSoup(open(page).read(),'html.parser')
 
-#Simple way to extract names and salaries from the html file
-name_box= soup.find_all('th' , attrs={'class' : 'left ' })
-sal_box = soup.find_all('td', attrs={'data-stat' : 'y1'})
+page = requests.get('https://www.basketball-reference.com/contracts/PHO.html').content
 
-my_player_base = Playerbase()  #an instance of my 'LinkedList' interpretation
+bs = BeautifulSoup(page, 'html5lib')
 
-#send all found attributes to create a 'Player' Node then add it to the LinkedList. May have to change this when we have more attributes.
-for _ in range(len(sal_box)):
-    my_player_base.insert(name_box[_].text.strip(), sal_box[_].text.strip())
+name_box= bs.find_all('th' , attrs={'class' : 'left ' })
+sal_box = bs.find_all('td', attrs={'data-stat' : 'y1'})
+age_box = bs.find_all('td', attrs={'data-stat' : 'age_today'})
 
-my_player_base.test_print() #Just see if this is working properly (Seems like it is)
+pb = Playerbase()
+
+for i in range(len(age_box)):
+    pb.insert(name_box[i].text.strip(), sal_box[i].text.strip()[1:], age_box[i].text.strip())
+
+pb.test_print()
+
