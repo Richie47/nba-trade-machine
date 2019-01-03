7d036d08afc275aefad1cbd1c334b241a4c71d46
Richie <richiemck47@gmail.com>
2019-01-03 00:11:33 -0700

    Update player_table.sql

--- player_table.sql
+++ player_table.sql
@@ -4,18 +4,31 @@ Uncomment the things below to create a new table
 
 -- Create a new table called 'NBA_TEAM' in schema 'dbo'
 -- Drop the table if it already exists
---IF OBJECT_ID('dbo.NBA_TEAM', 'U') IS NOT NULL
---DROP TABLE dbo.NBA_TEAM
---GO
--- Create the table in the specified schema
---CREATE TABLE dbo.NBA_TEAM
---TODO: CREATE A PRIMARY KEY
---(
---   Name      [NVARCHAR](50)   NOT NULL ,
---   Age      [NVARCHAR](50)  NOT NULL,
---   Salary   [NVARCHAR](50)  NOT NULL
---);
+IF OBJECT_ID('dbo.NBA_TEAM', 'U') IS NOT NULL
+DROP TABLE NBA_TEAM
+GO
+
+CREATE TABLE dbo.NBA_TEAM
+ (  ID [NVARCHAR](500) PRIMARY KEY,
+    Team [NVARCHAR](500),
+   Age [NVARCHAR](500),
+  Name [NVARCHAR](500),
+   Picture [NVARCHAR](500),
+   Salary  [NVARCHAR](500),
+   PPG   [NVARCHAR](500),
+  RPG [NVARCHAR](500),
+  APG [NVARCHAR](500),
+  PER [NVARCHAR](500)
+)
+
+
+/*
+Uncomment the things below to create a new table
+*/
+
+-- Create a new table called 'NBA_TEAM' in schema 'dbo'
+-- Drop the table if it already exists
 
 --Just run the table
-SELECT * FROM NBA_TEAM
+SELECT * FROM dbo.NBA_TEAM
 GO
