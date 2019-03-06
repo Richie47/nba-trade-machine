be4ea2fa1351509c6d21b7af3e42400172b74730
kalmasri123 <kalmasri@me.com>
2019-03-05 20:45:00 -0500

    Added warning message

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,19 +1,31 @@
+var selectedTeams = []
 window.onload = function () {
     var teams = document.getElementsByTagName('button');
     console.log("Hello")
-    var selectedTeams = []
+
     for (team in teams) {
         teams[team].onclick = function () {
             console.log(this.innerText)
             if (this.classList.contains('selected')) {
                 selectedTeams.splice(selectedTeams.indexOf(this.innerText), 1)
                 this.classList.remove('selected')
+                checkExceededLimit()
+
             } else {
                 selectedTeams.push(this.innerText)
 
                 this.className += ' selected'
+                checkExceededLimit()
             }
             console.log(selectedTeams)
         }
+    }
+}
+
+var checkExceededLimit = () => {
+    if (selectedTeams.length > 4) {
+        document.getElementById('errormessage').innerText = `You have too many Teams selected(${selectedTeams.length} teams).\n Please choose four or less`
+    } else {
+        document.getElementById('errormessage').innerText = `${selectedTeams.length} Team${ (selectedTeams.length!=1) ? 's':""} Selected`
     }
 }
