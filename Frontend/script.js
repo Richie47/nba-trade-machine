b6cc489f52a6754263cd71bd9f01c87cf92427b0
Richie <richiemck47@gmail.com>
2019-03-07 15:36:25 -0700

    Update script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,51 +1,32 @@
-/**
- * this function is responsible for handeling user selection of the teams for the homepage
- */
-window.onload = function () {
-    var reveal =document.querySelector(".trade-button");
-    var teams = document.getElementsByClassName('button'); //teams is all the team selection buttons on the webpage
-   
-    
-    var selectCount = 0; //keep track of team selection logic
-    const teamSelectLimit = 4; //set a hard limit of max teams to be selected at one time
-    var selectedTeams = [] 
-    
-    for (team in teams) {
-        teams[team].onclick = function () {
-            console.log(this.innerText)
-            //team is already selected but clicked again, we unselect the team and decrement the select count
-            if (this.classList.contains('selected')) {
-                selectedTeams.splice(selectedTeams.indexOf(this.innerText), 1)
-                this.classList.remove('selected')
-                this.style.backgroundColor = "#fff";
-                selectCount--;
-            }
-            //team is selected after the selection limit is reached, for now we just do nothing
-            //TODO: Implement a popup box notifying the user that they must unselect one team.
-            //TODO: Implement within this popup box a "don't show me this again" checkbox logic
-            else if(selectCount >= teamSelectLimit) {
-                checkExceededLimit();
-            }
-           
-            //otherwise we select the team and add to our selectCount
-            else{
-                selectedTeams.push(this.innerText)
-                this.style.backgroundColor = "rgb(233,233,233)";
-                this.className += ' selected'
-                reveal.style.display = "inline"
-                selectCount++;
-                
-            }
+window.onload = function(){
+  var selectCount = 0;
+  const teamSelectLimit = 4;
 
-            if(selectCount == 0){
-                reveal.style.display ="none";
-            }
-            console.log(selectedTeams)
-        };
-    }
+  $(document).ready(function(){
+  
+     $('.button').click(function(e){
+        e.preventDefault();  //catch something idk just copied this online
+
+        //so if the team was already selected we want to remove the selection
+        if(e.currentTarget.classList.contains('selected')){
+          e.currentTarget.classList.remove('selected'); //remove the selected class
+          e.currentTarget.style.backgroundColor = "#fff";
+          selectCount--;
+        //search the span ID, find the exact text of the image we inserted and remove it
+        $("#team-trades").find("img[src='" + $(e.currentTarget).find("img").attr("src") + "']").remove()
+         }
+         //if the user selects more then 4 teams we tell them this is not valid
+         // TODO: make a checkbox on the alert
+         else if(selectCount >= teamSelectLimit){
+           alert("You can only trade 4 teams maximum!");
+         }
+         //otherwise we add selected to the button's class and clone the image to the span
+         else{ 
+          e.currentTarget.className += ' selected';
+           e.currentTarget.style.backgroundColor = "rgb(233,233,233)";
+           selectCount++;
+           $("#team-trades").prepend($(e.currentTarget).find("img").clone().attr("height", 45).attr("width", 45))
+         } 
+        });
+});
 };
-
-var checkExceededLimit = () => {
-        var errorMsg = document.getElementById('error-message');
-        errorMsg.innerText = "You have too many Teams selected.\n Please choose four or less\nTODO MAKE THIS A POPUP!";
-        };
