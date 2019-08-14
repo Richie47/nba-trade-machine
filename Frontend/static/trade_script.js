38dca8015f9744336719f99266abe0c9b6c46bf0
Richie47 <RichieMck47@gmail.com>
2019-08-13 18:14:04 -0700

    early modal implementation, minor code cleanup

--- Frontend/static/trade_script.js
+++ Frontend/static/trade_script.js
@@ -1,4 +1,75 @@
+/**
+ *GLOBAL VARIABLES BELOW
+ */
+ //gets the selected teams from the index.html page.
+const teamDatabase = JSON.parse(localStorage.getItem('selectedTeams'));
+const teamsToCreate = teamDatabase.length ; //account for 0 index, integer of how many teams were selected from index.html
 
+ //set the tables that may get used(depending on # of teams selected), these are what the "roster sheets" are made of
+//some of these variables may not be used
+const firstTeam = document.querySelector('.playerTable1');
+const secondTeam = document.querySelector('.playerTable2');
+const thirdTeam = document.querySelector('.playerTable3');
+const forthTeam = document.querySelector('.playerTable4');
+//keep array, will match the index of the length of team rosters we need to make.
+const teamTables = [firstTeam, secondTeam, thirdTeam, forthTeam];
+let playerSelected = document.querySelectorAll("[class^='playerTable']");
+let tradeBlock = [];
+let newTradeBlock = [];
+
+const modal = document.querySelector(".trade-modal");
+const closeButton = document.querySelector(".close-button");
+closeButton.addEventListener("click", toggleModal);
+
+/*  END OF GLOBAL VARIABLES */
+
+/* GLOBAL PROCESSES BELOW */
+for(let i = 0; i < teamsToCreate; i++) {
+    generateRosterHeader(teamDatabase[i], i );
+     //get path to our JSON database. I wanted to not use jQuery but it was just really easy here.
+    $.getJSON("./players.json", function (teams) {
+        const players = teams.filter(team => team.Team.match(teamDatabase[i]));
+        generatePlayerTable(teamTables[i], players);
+        generatePlayerTableHead(teamTables[i], players, teamDatabase[i]);
+    });
+}
+
+for(const tr of playerSelected){
+    tr.addEventListener('click', function(e){
+        const clickedPlayer = e.path[1].querySelectorAll('td')[0].innerHTML;
+        tr.addEventListener("click", toggleModal)
+        if(e.path[1].classList.contains('tradeSelected')){
+            console.log('yoo')
+            e.path[1].classList.remove('tradeSelected')
+            newTradeBlock = tradeBlock.filter((player) => player != clickedPlayer);
+            tradeBlock = newTradeBlock;
+            //console.log(newTradeBlock)
+             document.querySelector(".confirm-trade").innerHTML = newTradeBlock.join("");
+        }
+        else{
+            console.log('in else')
+            e.path[1].classList.add('tradeSelected');
+            tradeBlock.push(clickedPlayer);
+            //console.log(tradeBlock)
+            document.querySelector(".confirm-trade").innerHTML = tradeBlock.join("");
+        }
+        console.log(e.path[4])
+        let demo = e.path[4].innerHTML.toString()
+        //demo = demo.substring(0,50)
+        const res = demo.match(/playerTable.?/g);
+        console.log(res.toString())
+//        console.log(e.target.parentElement.parentElement.parentElement)
+        //console.log(x.replace(/\D/g, ''))  regex for removing currency format on salary
+    })
+}
+
+/*END GLOBAL PROCESSES*/
+
+function toggleModal(){
+    modal.classList.toggle("show-modal")
+}
+
+//used to take off the currency format of the player's salaries for the trade logic
 const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
@@ -39,20 +110,6 @@ function generatePlayerTable(table, data){
 
 }
 
-  //event listener for second page
-$(document).ready(function(){
-    $('.test').click(function(e){
-      e.preventDefault();
-      //gets the selected teams from the index.html page.
-      var test = JSON.parse(localStorage.getItem('selectedTeams'));
-      //get path to our JSON database
-      $.getJSON("./players.json", function(teams){
-      var x = teams.filter(team => team.Team.match(test[0]));
-      console.log(x);
-    });
-    });
-  });
-
 function generateRosterHeader(teamName, index){
     const divs = document.getElementsByClassName("team-salary");
     let rosterHeader =  divs[index];
@@ -71,75 +128,18 @@ function generateRosterHeader(teamName, index){
     });
 }
 
+/**TODO: Honestly don't know wtf this code does, figure out if we need it still
+  //event listener for second page
+$(document).ready(function(){
+    $('.test').click(function(e){
+      e.preventDefault();
       //gets the selected teams from the index.html page.
-const teamDatabase = JSON.parse(localStorage.getItem('selectedTeams'));
-const teamsToCreate = teamDatabase.length ; //account for 0 index
-console.log(teamsToCreate);
-
- //set the tables that may get used
-const firstTeam = document.querySelector('.playerTable1');
-const secondTeam = document.querySelector('.playerTable2');
-const thirdTeam = document.querySelector('.playerTable3');
-const forthTeam = document.querySelector('.playerTable4');
-//keep array, will match the index of the length of team rosters we need to make.
-let teamTables = [firstTeam, secondTeam, thirdTeam, forthTeam];
-
-
-/**
- * so it's been a while. We got the roster pic + salary cap like a week ago.
- *
- * The last of our UI which does tie into trade logic. We need to be able to click a player and well two
- * 1) have the player, picture, and salary neatly put above a different team
- * 2) create some form of interface that allows you to choose which team for multi team cases.
- * bonus if you only make itnerface work for 3+ teams only. We'll leave out the trade logic for this moment, but keep in mind we probably
- * want a counter of the salary traded.
- *
- * I was thinking of adding when clicked to a special selector.
- * First of all, we need to figure out how to figure out what table the player selected came from.
- * Then we need to think of how we're going to get him on top of another team. For now let's just worry about 2 team case.
- *
- *
- *
- */
-for(let i = 0; i < teamsToCreate; i++) {
-    generateRosterHeader(teamDatabase[i], i );
-     //get path to our JSON database. I wanted to not use jQuery but it was just really easy here.
-    $.getJSON("./players.json", function (teams) {
-        const players = teams.filter(team => team.Team.match(teamDatabase[i]));
-        generatePlayerTable(teamTables[i], players);
-        generatePlayerTableHead(teamTables[i], players, teamDatabase[i]);
+      const chosenTeams= JSON.parse(localStorage.getItem('selectedTeams'));
+      //get path to our JSON database
+      $.getJSON("./players.json", function(teams){
+      let numberOfFoundTeams = teams.filter(team => team.Team.match(chosenTeams[0]));
+      console.log("yo" + numberOfFoundTeams);
     });
-}
-
-let playerSelected = document.querySelectorAll("[class^='playerTable']");
-let tradeBlock = [];
-let newTradeBlock = [];
-
-for(const tr of playerSelected){
-    tr.addEventListener('click', function(e){
-        const clickedPlayer = e.path[1].querySelectorAll('td')[0].innerHTML;
-        if(e.path[1].classList.contains('tradeSelected')){
-            console.log('yoo')
-            e.path[1].classList.remove('tradeSelected')
-            newTradeBlock = tradeBlock.filter((player) => player != clickedPlayer);
-            tradeBlock = newTradeBlock;
-            //console.log(newTradeBlock)
-             document.querySelector(".confirm-trade").innerHTML = newTradeBlock.join("");
-        }
-        else{
-            console.log('in else')
-            e.path[1].classList.add('tradeSelected');
-            tradeBlock.push(clickedPlayer);
-            //console.log(tradeBlock)
-            document.querySelector(".confirm-trade").innerHTML = tradeBlock.join("");
-        }
-        console.log(e.path[4])
-        let demo = e.path[4].innerHTML.toString()
-        //demo = demo.substring(0,50)
-        const res = demo.match(/playerTable.?/g);
-        console.log(res.toString())
-//        console.log(e.target.parentElement.parentElement.parentElement)
-        //console.log(x.replace(/\D/g, ''))  regex for removing currency format on salary
-    })
-}
-
+    });
+  });
+ **/
