5c44a0ae3571c752124325c9486aa61805b06c4a
Richie47 <RichieMck47@gmail.com>
2020-01-11 20:07:54 -0700

    heroku setup procfile

--- Frontend/express_tester.js
+++ Frontend/express_tester.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var http = require('http');
-var app = express();
-var path = require('path');
-var reload = require('reload'); //npm install -g --save-dev reload
+let express = require('express');
+const http = require('http');
+let PORT = process.env.PORT || 5000;
+const app = express();
+const path = require('path');
+const reload = require('reload'); //npm install -g --save-dev reload
 app.use(express.static(path.join(__dirname, "static")));
 app.use(express.static(path.join(__dirname,'./images')));
 app.use(express.static(path.join(__dirname, './logos')));
@@ -12,18 +13,18 @@ app.use(express.static(path.join(__dirname, './salary_cap')));
 var server = http.createServer(app);
 //reload code to refresh browser upon code completion
 reload(app).then(function (reloadReturned) {
-var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
-    //for heroku
-    //const port = process.env.PORT || 3000
+    const server = app.listen(PORT, function () {
+        const host = server.address().address;
+        const port = server.address().port;
+        //for heroku
+        //const port = process.env.PORT || 3000
 
-    console.log("Example app listening at http://%s:%s", host, port);
+        console.log("Example app listening at http://%s:%s", host, port);
 
-    if (process.send){
-        process.send('online');
-    }
-});
+        if (process.send) {
+            process.send('online');
+        }
+    });
 }).catch(function (err) {
     console.error('Reload could not start, could not start server/sample app', err)
 });
